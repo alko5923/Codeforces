4027126cc2e69347e304cd83a959370d16425892
Aljaz Kovac <aljazkovac@Aljazs-MacBook-Pro.local>
2022-07-26 00:40:50 +0200

    added a comment

--- leetcode-csharp/leetcode-number-of-1-bits.cs
+++ leetcode-csharp/leetcode-number-of-1-bits.cs
@@ -2,6 +2,10 @@ public class SolutionBits
 {
     public int HammingWeight(uint n)
     {
+        // Solved with the Brian Kerninghan algorithm. 
+        // The algorithms unsets the rightmost bit, 
+        // thus running for as many iterations as there
+        // are set bits. 
         int count = 0;
         while (n!=0)
         {
